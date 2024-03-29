1656e0c4077255fa8a9cc88aa5c6d34133d17c7e
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2024-03-29 15:45:54 +0800

    Add files via upload

--- Ast/Script/Javascript/script.js
+++ Ast/Script/Javascript/script.js
@@ -16,6 +16,8 @@ function toggleSidebar() {
     }
 }
 
+var copyright = document.getElementById('ftrcopy');
+copyright.innerHTML = "Copyright 2024 | Design by Dyn Crazy"
 
 
 const textToType = "WELCOME!";
@@ -42,5 +44,22 @@ function toggleProject(divId) {
     const div = document.getElementById(divId);
     div.classList.toggle('hidden');
 }
+
+function updateClock() {
+    var currentTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
+
+    var dateTimeParts = currentTime.split(', ');
+    var dayAbbreviation = dateTimeParts[0].split(' ')[0];
+    var dateMonthYear = dateTimeParts[0].split(' ')[1];
+    var time = dateTimeParts[1];
+
+    var formattedDateTime = dayAbbreviation + ' - ' + time;
+
+    document.getElementById('clock').innerHTML = formattedDateTime;
+
+    setTimeout(updateClock, 1000);
+}
+
+updateClock();
 
 console.log('Masih dalam tahap pengembangan...')
