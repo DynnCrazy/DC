af021ff162f304c24de50b7e20ab216f78dcfa2b
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2024-04-10 13:29:31 +0800

    Add files via upload

--- Ast/Script/Javascript/script.js
+++ Ast/Script/Javascript/script.js
@@ -4,6 +4,50 @@
 //    navbarLinks.style.display = (navbarLinks.style.display === 'none' || navbarLinks.style.display === '') ? 'flex' : 'none';
 // }
 // == Masih pemula aku puh
+  
+const toggleButton = document.getElementById('theme-toggle');
+        const body = document.body;
+
+        // Check if user has previously selected a theme
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            body.classList.add(savedTheme);
+        }
+
+        // Function to toggle between dark and light mode
+        function toggleMode() {
+            if (body.classList.contains('dark-mode')) {
+                body.classList.replace('dark-mode', 'light-mode');
+                localStorage.setItem('theme', 'light-mode');
+                
+                var darkandlight = document.getElementById("logolandd");
+                darkandlight.innerHTML = '<i class="fa fa-moon-o"></i>';
+
+            } else {
+                body.classList.replace('light-mode', 'dark-mode');
+                localStorage.setItem('theme', 'dark-mode');
+              
+                var darkandlight = document.getElementById("logolandd");
+                darkandlight.innerHTML = '<i class="fa fa-sun-o"></i>';
+            }
+        }
+
+        // Event listener for theme toggle button
+        toggleButton.addEventListener('click', toggleMode);
+
+function xoggleMode() {
+    alert("Segera!");
+}
+
+function openSearchCont() {
+    var searchDiv = document.getElementById('searchDiv');
+    searchDiv.style.display = 'block';
+}
+
+function closeSearchCont() {
+    var searchDiv = document.getElementById('searchDiv');
+    searchDiv.style.display = 'none';
+}
 
 function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
