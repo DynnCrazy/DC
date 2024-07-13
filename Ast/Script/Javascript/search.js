e0a1f067fa425d9a9cebb66ce8c8009720dbf9e4
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2024-07-13 15:25:18 +0800

    Add files via upload

--- Ast/Script/Javascript/search.js
+++ Ast/Script/Javascript/search.js
@@ -1,44 +1,46 @@
 // Data yang akan Anda cari (contoh: daftar nama dan link)
 const data = [
     // Main
-    { name: "Menu", link: "/index.html", klik: '-' },
-    { name: "Skill", link: "/index.html#skill", klik: '-' },
-    { name: "Project", link: "/index.html#project", klik: '-' },
-    { name: "Collection", link: "/index.html#collection", klik: '-' },
-    { name: "About Me", link: "/index.html#about", klik: '-' },
-    { name: "Contact", link: "/index.html#contact", klik: '-' },
+    { name: "Menu", link: "/index.html", keyword: "menu utama index" },
+    { name: "Skill", link: "/index.html#skill", keyword: "my skill kemampuan saya" },
+    { name: "Project", link: "/index.html#project", keyword: "my project projek saya" },
+    { name: "Collection", link: "/index.html#collection", keyword: "my collection koleksi saya" },
+    { name: "About Me", link: "/index.html#about", keyword: "about me tentang saya" },
+    { name: "Contact", link: "/index.html#contact", keyword: "contact me kontak saya email wa no" },
+    //  Blog
+    { name: "Blog", link: "/Blog/index.html#", keyword: "my blog saya" },
     // My Film List
-    { name: "My Film List", link: "/Collection/Film/index.html", klik: '-' },
+    { name: "My Film List", link: "/Collection/Film/index.html", keyword: "my film list movie kartun animasi anime daftar tonton saya" },
     // My Game List
-    { name: "My Game List", link: "/Collection/Game/index.html", klik: '-' },
-    { name: "Free Fire", link: "/Collection/Game/index.html#free_fire", klik: '-' },
-    { name: "Mobile Legends", link: "/Collection/Game/index.html#mobile_legends", klik: '-' },
-    { name: "FC Mobile", link: "/Collection/Game/index.html#fc_mobile", klik: '-' },
-    { name: "Subway Surfers", link: "/Collection/Game/index.html#subway_surfers", klik: '-' },
-    { name: "Stumble Guys", link: "/Collection/Game/index.html#stumble_guys", klik: '-' },
-    { name: "Pou", link: "/Collection/Game/index.html#pou", klik: '-' },
-    { name: "Among Us", link: "/Collection/Game/index.html#among_us", klik: '-' },
-    { name: "Blood Strike", link: "/Collection/Game/index.html#blood_strike", klik: '-' },
-    { name: "Citampi Stories", link: "/Collection/Game/index.html#citampi_stories", klik: '-' },
+    { name: "My Game List", link: "/Collection/Game/index.html", keyword: "my game list daftar permainan saya" },
+    { name: "Free Fire", link: "/Collection/Game/index.html#free_fire", keyword: "game free fire burik hd 8bit garena ff epep bocil" },
+    { name: "Mobile Legends : Bang Bang", link: "/Collection/Game/index.html#mobile_legends", keyword: "ml game mobile legends : bang bang mati suri mpl moba analog moonton" },
+    { name: "FC Mobile", link: "/Collection/Game/index.html#fc_mobile", keyword: "game fc mobile sepak bola fifa messi ronaldo soccer ea electronic arts" },
+    { name: "Subway Surfers", link: "/Collection/Game/index.html#subway_surfers", keyword: "game subway surfers" },
+    { name: "Stumble Guys", link: "/Collection/Game/index.html#stumble_guys", keyword: "game stumble guys fall guys" },
+    { name: "Pou", link: "/Collection/Game/index.html#pou", keyword: "game pou tai" },
+    { name: "Among Us", link: "/Collection/Game/index.html#among_us", keyword: "game among us sus fitnah super imposter" },
+    { name: "Blood Strike", link: "/Collection/Game/index.html#blood_strike", keyword: "game blood strike" },
+    { name: "Citampi Stories", link: "/Collection/Game/index.html#citampi_stories", keyword: "game citampi stories story indonesia lokal ikan asin production" },
 
-    { name: "Grand Theft Auto: San Andreas", link: "/Collection/Game/index.html#gta_sa", klik: '-' },
+    { name: "Grand Theft Auto: San Andreas", link: "/Collection/Game/index.html#gta_sa", keyword: "game grand theft auto : san andreas gta sa rockstar games" },
 
-    { name: "Doodle Champions Island", link: "/Collection/Game/index.html#doodle_champions_island", klik: '-' },
+    { name: "Doodle Champions Island", link: "/Collection/Game/index.html#doodle_champions_island", keyword: "game doodle champions island" },
     // Social Media
-    { name: "YouTube", link: "/Links/youtube.html", klik: '-' },
-    { name: "Instagram", link: "/Links/instagram.html", klik: '-' },
-    { name: "TikTok", link: "/Links/tiktok.html", klik: '-' },
-    { name: "Facebook", link: "/Links/facebook.html", klik: '-' },
-    { name: "GitHub", link: "/Links/github.html", klik: '-' },
+    { name: "YouTube", link: "/Links/youtube.html", keyword: "yt youtube saya dyn crazy official resmi" },
+    { name: "Instagram", link: "/Links/instagram.html", keyword: "ig instagram saya dyn crazy official resmi" },
+    { name: "TikTok", link: "/Links/tiktok.html", keyword: "tt tik tok tiktok saya dyn crazy official resmi" },
+    { name: "Facebook", link: "/Links/facebook.html", keyword: "fb facebook saya dyn crazy official resmi" },
+    { name: "GitHub", link: "/Links/github.html", keyword: "gh github saya dyn crazy official resmi" },
     // Web Structure
-    { name: "Top Web", link: "#topWeb", klik: '-' },
-    { name: "Center Web", link: "#centerWeb", klik: '-' },
-    { name: "Buttom Web", link: "#buttomWeb", klik: '-' },
+    { name: "Top Web", link: "#topWeb", keyword: "top web atas" },
+    { name: "Center Web", link: "#centerWeb", keyword: "center web tengah" },
+    { name: "Buttom Web", link: "#buttomWeb", keyword: "buttom web bawah" },
     // Function
-    { name: "Hidden Content", link: "?content=none", klik: '-' },
-    { name: "Hello World!", link: "javascript:alert('Hello World!');", klik: '-' },
+    { name: "Hidden Content", link: "?content=none", keyword: "sembunyikan konten hidden content function heker" },
+    { name: "Hello World!", link: "javascript:alert('Hello World!');", keyword: "hello world" },
     // End
-    { name: "End", link: "/easter-egg-in-here.html", klik: '-' }
+    { name: "End", link: "/easter-egg-in-here.html", keyword: "123456789" }
 ];
 
 // Gak ada gunanya
@@ -65,7 +67,7 @@ function search() {
     }
 
     // Filter data sesuai dengan input
-    const results = data.filter(item => item.name.toLowerCase().includes(input));
+    const results = data.filter(item => item.keyword.toLowerCase().includes(input)); // const results = data.filter(item => item.name.toLowerCase().includes(input));
 
     // Tampilkan hasil pencarian
     results.forEach(result => {
@@ -87,7 +89,7 @@ document.getElementById("searchInput").addEventListener("input", search);
 
 // Placeholder
 const inputField = document.getElementById('searchInput');
-const placeholders = ['Contact', 'Siapa Dyn Crazy ?', 'YouTube', 'Grand Theft Auto: San Andreas', 'Cara cepat miskin bagi pemula', 'Film List', 'TikTok', 'Game burik no 1 di dunia', 'Tips & trick jadi pengangguran', 'Skill', 'Kenapa cowok selalu salah ?', 'Cara membedakan kang bakso dengan intel!', 'Video viral terbaru! link mediafire!', 'Project', 'Mobile Legends: Bang Bang', 'Tutorial jadi badut untuk cewek! 100% work!', 'Kenapa upin & ipin tidak naik kelas ?', 'Blog', 'Easter Egg', 'Ronaldo Vs Messi', 'Among Us', 'Heboh! Prilly Latuconsina makan pakai tangan!', '#FREEPALESTINE', 'Bahasa pemograman Python', 'Duluan ayam apa telur ?', 'Ah ah ah', 'Indonesia merdeka tahun 1945', 'Berapa jumlah provinsi di Indonesia ?', 'So.. bakso..', 'Instagram', 'Kenapa cewek jepang `mantap` ?', 'Bocil epep meresahkan dunia!'];
+const placeholders = ['Contact', 'Siapa Dyn Crazy ?', 'YouTube', 'Grand Theft Auto: San Andreas', 'Cara cepat miskin bagi pemula', 'Film List', 'TikTok', 'Game burik no 1 di dunia', 'Tips & trick jadi pengangguran', 'Skill', 'Kenapa cowok selalu salah ?', 'Cara membedakan kang bakso dengan intel!', 'Ketikan sesuatu disini!', 'Project', 'Mobile Legends: Bang Bang', 'Tutorial jadi badut untuk cewek! 100% work!', 'Kenapa upin & ipin tidak naik kelas ?', 'Blog', 'Easter Egg', 'Ronaldo Vs Messi', 'Among Us', 'Heboh! Prilly Latuconsina makan pakai tangan!', '#FREEPALESTINE', 'Bahasa pemograman Python', 'Duluan ayam apa telur ?', 'Ah ah ah', 'Indonesia merdeka tahun 1945', 'Berapa jumlah provinsi di Indonesia ?', 'So.. bakso..', 'Instagram', 'Bocil epep meresahkan dunia!'];
 let currentIndex = 0;
 let placeholderInterval;
 
